a66e9257ef70500a6d8c8ca90a4ef1b3451d3403
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2024-03-11 01:38:31 +0800

    adjusted space

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <header>
               <div className="flex items-center justify-between my-5 text-md">
                 <Link href="/" className="font-semibold">George&apos;s Safe Space</Link>
-                <nav className="ml-auto font-medium space-x-6 flex items-center justify-between">
+                <nav className="ml-auto text-sm font-medium space-x-3 flex items-center justify-between">
                   <ModeToggle />
                   <LinkedInLogo link="https://www.linkedin.com/in/georgetanjs"/>
                   <GitHubLogo link="https://github.com/GeorgeTan615"/>
-                  <Link href="/">Home</Link>
                   <Link href="/about">About</Link>
                 </nav>
               </div>
