0e904437595e828f5dc0ab32563c3732e0e3ad87
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2024-03-17 19:26:25 +0800

    added time to read

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -53,9 +53,14 @@ export default async function PostPage({ params }: PostProps) {
     <article className="py-6 prose dark:prose-invert">
       <h1 className="mb-2">{post.title}</h1>
       {post.description && (
-        <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
-          {post.description}
-        </p>
+        <div className="flex justify-between items-center my-0">
+          <div className="text-md text-slate-700 dark:text-slate-200 ">
+            {post.description}
+          </div>
+          <div className="text-xs text-slate-700 dark:text-slate-200">
+            {post.readTimeMinutes}
+          </div>
+        </div>
       )}
       <hr className="my-4" />
       <Mdx code={post.body.code} />
--- contentlayer.config.js
+++ contentlayer.config.js
@@ -12,6 +12,18 @@ const computedFields = {
   },
 }
 
+export const calculateReadingTime = (text) => {
+  // Step 2: Determine the average reading speed (words per minute)
+  const wordsPerMinute = 200;
+  // Step 3: Calculate the word count
+  const noOfWords = text.split(/\s/g).length;
+  // Step 4: Calculate the estimated reading time (in minutes)
+  const minutes = noOfWords / wordsPerMinute;
+  const readTime = Math.ceil(minutes);
+  // Step 5: Format the output
+  return `${readTime} minute${readTime > 1 ? "s" : ""} read`;
+}
+
 export const Page = defineDocumentType(() => ({
   name: "Page",
   filePathPattern: `pages/**/*.mdx`,
@@ -43,9 +55,15 @@ export const Post = defineDocumentType(() => ({
     date: {
       type: "date",
       required: true,
-    }
+    },
   },
-  computedFields,
+  computedFields: {
+    readTimeMinutes: {
+      type: "string",
+      resolve: (doc) => calculateReadingTime(doc.body.raw)
+    },
+    ...computedFields
+  },
 }))
 
 export default makeSource({
